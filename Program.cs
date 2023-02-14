c209d8e9c145b61471c40331b6b571c1bf2acd55
ArthurGalanti <arthur.galanti@hotmail.com>
2023-02-14 08:05:30 -0300

    Configurações iniciais

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using SeleniumBot.Data;
+using SeleniumBot.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<AppDataContext>();
+builder.Services.AddScoped<ScrapperService>();
+builder.Services.AddSingleton<CustomizeService>();
+
+
+builder.Services.AddCors(policeBuilder =>
+    policeBuilder.AddDefaultPolicy(policy => policy.WithOrigins("*").AllowAnyHeader().AllowAnyMethod()));
+
+builder.WebHost.UseUrls("http://localhost:4000", "http://odin:4000", "http://192.168.15.108:4000");
+
+var app = builder.Build();
+app.UseCors();
+app.MapControllers();
+app.Run();
+
+
+
