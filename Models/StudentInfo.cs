ee0464d160f6182228586d775ce54f26fdd88391
ArthurGalanti <arthur.galanti@hotmail.com>
2023-02-15 10:45:49 -0300

    Adicionado model StudentInfo

---
+++ Models/StudentInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace SeleniumBot.Models;
+
+public class StudentInfo
+{
+    public string? Name { get; set; }
+    public string? Course { get; set; }
+    public string? Period { get; set; }
+}
