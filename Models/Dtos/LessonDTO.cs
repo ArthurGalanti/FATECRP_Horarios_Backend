cef1141ccf1940a896cc1eb18c27c0165eac0034
ArthurGalanti <arthur.galanti@hotmail.com>
2023-02-16 08:15:11 -0300

    Adicionado DTO LessonDTO

---
+++ Models/Dtos/LessonDTO.cs
@@ -0,0 +1,18 @@
+﻿namespace SeleniumBot.Models.Dtos;
+
+public class LessonDTO
+{
+    public string? WeekDay { get; set; }
+    public List<LessonProperties>? Properties { get; set; }
+    
+}
+
+public struct LessonProperties
+{
+    public string? TeacherName { get; set; }
+    public string? LessonTime { get; set; }
+    public string? SubjectName { get; set; }
+    public string? Semester { get; set; }
+    public string? Classroom { get; set; }
+    public string? Location { get; set; }
+}
