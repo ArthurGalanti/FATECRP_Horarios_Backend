553681ededc9458ce72dce0764d906ec8f0f499a
ArthurGalanti <arthur.galanti@hotmail.com>
2023-02-17 15:06:59 -0300

    Adicionado AppContext

---
+++ Data/AppDataContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SeleniumBot.Data.Mapping;
+using SeleniumBot.Models;
+
+namespace SeleniumBot.Data;
+public class AppDataContext : DbContext
+{
+    public DbSet<Lesson> Lessons { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlServer("Server=DEVELOPMENT;Database=FatecDB;Integrated Security=True;TrustServerCertificate=True");
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new LessonMap());
+    }
+
+    
+}
