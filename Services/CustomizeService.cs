9e04a9e9005c847ef659b57756015ba35f1ee096
ArthurGalanti <arthur.galanti@hotmail.com>
2023-02-23 10:27:18 -0300

    Adicionado Service CustomizeService

---
+++ Services/CustomizeService.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+using SeleniumBot.Models;
+using SeleniumBot.Models.Dtos;
+
+namespace SeleniumBot.Services;
+
+public class CustomizeService
+{
+    public List<LessonDTO> OrderByWeekday(List<Lesson> gradeTable)
+    {
+        var weekdayMap = new List<string> {"Segunda-Feira","Terça-Feira","Quarta-Feira","Quinta-Feira", "Sexta-Feira", "Sábado"};
+        var currentDayOfWeek = DateTime.Now.DayOfWeek;
+        var textInfo = new CultureInfo("pt-BR",false).TextInfo;
+        var currentDayName = textInfo.ToTitleCase(DateTimeFormatInfo.CurrentInfo.GetDayName(currentDayOfWeek).ToLower());
+        weekdayMap.Remove(currentDayName);
+        weekdayMap.Insert(0, currentDayName + "- (Hoje)");
+
+        return (from day in weekdayMap
+            let lessonsDay = gradeTable.Where(x => x.WeekDay == day.Replace("- (Hoje)", ""))
+                .OrderBy(x => x.LessonTime)
+                .Select(x => new LessonProperties
+                {
+                    TeacherName = x.TeacherName,
+                    LessonTime = x.LessonTime,
+                    SubjectName = x.SubjectName,
+                    Semester = x.Semester,
+                    Classroom = $"{x.Classroom} - {x.Floor}",
+                    Location = x.LocationURL
+                })
+                .ToList()
+            select new LessonDTO() { WeekDay = day, Properties = lessonsDay }).ToList();
+    }
+}
